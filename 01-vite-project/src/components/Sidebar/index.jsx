b0a6fda11e902afc2a724f380e00a2408e87eb99
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-12 20:02:39 -0300

    feat: Finishing Sidebar Component

--- 01-vite-project/src/components/Sidebar/index.jsx
+++ 01-vite-project/src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export const Sidebar = () => {
@@ -8,11 +10,13 @@ export const Sidebar = () => {
             src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/54908803?v=4" />
                 <strong>Leonardo Barbosa</strong>
                 <span>Web Developer</span>
             </div>
             <footer className={styles.footer}>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
