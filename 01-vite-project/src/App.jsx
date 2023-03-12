ae7dfa460333061cf7a11e1ff7fdd0b2be4e93d6
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-12 19:45:31 -0300

    refactor: global css edit

--- 01-vite-project/src/App.jsx
+++ 01-vite-project/src/App.jsx
@@ -1,12 +1,37 @@
-import { Header } from "./components/Header/Header"
+import { Header, Sidebar } from "@Components/index"
 
 import './global.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
     <Header />
-    <h1>Hello World</h1>
+    <div className={styles.wrapper}>
+      <Sidebar />
+
+      <main>
+      <div>
+        <p>Leonardo</p>
+        <span>Lorem ipsum dolor, 
+          sit amet consectetur adipisicing elit.
+           Voluptates in cum culpa rerum porro amet veritatis
+           tempora quidem cumque esse, repellendus eveniet hic error. 
+           Recusandae voluptatibus id aut esse pariatur.
+           </span>
+      </div>
+
+      <div>
+        <p>Leonardo</p>
+        <span>Lorem ipsum dolor, 
+          sit amet consectetur adipisicing elit.
+           Voluptates in cum culpa rerum porro amet veritatis
+           tempora quidem cumque esse, repellendus eveniet hic error. 
+           Recusandae voluptatibus id aut esse pariatur.
+           </span>
+      </div>
+      </main>
+    </div>
     </>
   )
 }
