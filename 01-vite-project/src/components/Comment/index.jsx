cf38f6aac975175640fc322662e6e34227c936e6
leoobarbosa2 <leonardobarbosa@MacBook-Pro-de-Leonardo.local>
2023-03-13 17:38:05 -0300

    feat: Like count feature

--- 01-vite-project/src/components/Comment/index.jsx
+++ 01-vite-project/src/components/Comment/index.jsx
@@ -1,11 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from '@Components/index';
 import * as styles from './Comment.module.css';
+import { useState } from 'react';
 
 export const Comment = ({ content, onDeleteComment }) => {
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleIncreaseLikeCount() {
+        setLikeCount(previousCount => previousCount + 1);
     }
 
     return (
@@ -30,9 +36,9 @@ export const Comment = ({ content, onDeleteComment }) => {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleIncreaseLikeCount}>
                         <ThumbsUp size={20} />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
